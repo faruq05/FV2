c9882904b61eb9819a000d76e511206cb8fd90f4
Omor Faruq <faruq05.fiverr@gmail.com>
2023-02-06 14:55:45 +0600

    Done

--- sendmail.php
+++ sendmail.php
@@ -1,63 +1,21 @@
 <?php
-if (isset($_POST['email'])) {
+if (isset($_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "omor6263@email.com";
-    $email_subject = "Website Contact Form:  " . $_POST['name'];
+  $to = "omor6263@gmail.com";
+  $headers = "From: $email" . "\r\n" .
+    "Reply-To: $email" . "\r\n" .
+    "X-Mailer: PHP/" . phpversion();
 
-    function died($error)
-    {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error . "<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
-
-    // validation expected data exists
-    if (
-        !isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message'])
-    ) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');
-    }
-
-    $name = $_POST['name']; // required
-    $email_from = $_POST['email']; // required
-    $message = $_POST['message']; // required
-
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-    if (!preg_match($email_exp, $email_from)) {
-        $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if (!preg_match($string_exp, $name)) {
-        $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-    }
-
-    if (strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br />';
-    }
-
-    if (strlen($error_message) > 0) {
-        died($error_message);
-    }
-
-    $email_message = "Form details below.\n\n";
-
-    function clean_string($string)
-    {
-        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-        return str_replace($bad, "", $string);
-    }
-
-    $email_message .= "Name: " . clean_string($name) . "\n";
-    $email_message .= "Email: " . clean_string($email_from) . "\n";
+  if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully.";
+  } else {
+    echo "Failed to send email.";
+  }
+} else {
+  echo "Invalid request.";
 }
 ?>
